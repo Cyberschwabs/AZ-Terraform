24a5ab42774df70b36d2df26f4a5afacae6e34f2
cyberschwabs <cyberschwabs@gmail.com>
2024-10-23 14:15:57 -0400

    updated pipeline and TF Files

--- tf-config/providers.tf
+++ tf-config/providers.tf
@@ -15,4 +15,7 @@ terraform {
   provider "azurerm" {
     features {}
     subscription_id = var.subscription_id
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.client_tenant
   }
